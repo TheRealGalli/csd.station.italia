478ddfda3f208917bfc33df3259eeefa94974dc7
Carlo Galli <carlogalli@MacBook-Air.local>
2025-11-15 03:22:27 +0100

    Fix Vite base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Base è impostata dinamicamente in CI tramite VITE_BASE_PATH
 export default defineConfig({
+	base: '/csd.station.italia/',
 	plugins: [react()],
-	base: process.env.VITE_BASE_PATH || '/',
 });
 
 
